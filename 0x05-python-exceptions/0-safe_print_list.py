192931fe4c1645b0fcebdb16f9a786819b670e9c
Cappu123 <cappyalemayehu12@gmail.com>
2023-12-18 13:41:39 -0800

    comit my solutions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+def safe_print_list(my_list=[], x=0)
+    count = 0
+    for i in my_list:
+            print(i, end = ' ')
+            count += 0
+            if count >= x
+                break
+            print('\n')
+    return count
