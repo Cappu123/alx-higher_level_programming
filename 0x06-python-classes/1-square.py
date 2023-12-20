ccad21aad738379d00b4e726b0e0477d900ba981
Cappu123 <cappyalemayehu12@gmail.com>
2023-12-19 17:26:13 -0800

    first 2 tasks are easy

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""python oop"""
+
+
+class square():
+    """class representing a square"""
+
+    def__init__(self, size)
+    """initializing the class"""
+
+    self.size = size
