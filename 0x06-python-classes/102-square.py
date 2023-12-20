f476736b55389f178994a40e518a5f0833e44259
Cappu123 <cappyalemayehu12@gmail.com>
2023-12-19 18:56:28 -0800

    Print Square instance

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,80 +1,55 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 
-"""Define classes for a singly-linked list."""
 
+class Square:
+    """Represent a square."""
 
-class Node:
-    """Represent a node in a singly-linked list."""
-
-    def __init__(self, data, next_node=None):
-        """Initialize a new Node.
+    def __init__(self, size=0):
+        """Initialize a new square.
 
         Args:
-            data (int): The data of the new Node.
-            next_node (Node): The next node of the new Node.
+            size (int): The size of the new square.
         """
-        self.data = data
-        self.next_node = next_node
+        self.size = size
 
     @property
-    def data(self):
-        """Get/set the data of the Node."""
-        return (self.__data)
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
 
-    @data.setter
-    def data(self, value):
+    @size.setter
+    def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("data must be an integer")
-        self.__data = value
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-    @property
-    def next_node(self):
-        """Get/set the next_node of the Node."""
-        return (self.__next_node)
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
-    @next_node.setter
-    def next_node(self, value):
-        if not isinstance(value, Node) and value is not None:
-            raise TypeError("next_node must be a Node object")
-        self.__next_node = value
+    def __eq__(self, other):
+        """Define the == comparision to a Square."""
+        return self.area() == other.area()
 
+    def __ne__(self, other):
+        """Define the != comparison to a Square."""
+        return self.area() != other.area()
 
-class SinglyLinkedList:
-    """Represent a singly-linked list."""
+    def __lt__(self, other):
+        """Define the < comparison to a Square."""
+        return self.area() < other.area()
 
-    def __init__(self):
-        """Initalize a new SinglyLinkedList."""
-        self.__head = None
+    def __le__(self, other):
+        """Define the <= comparison to a Square."""
+        return self.area() <= other.area()
 
-    def sorted_insert(self, value):
-        """Insert a new Node to the SinglyLinkedList.
+    def __gt__(self, other):
+        """Define the > comparison to a Square."""
+        return self.area() > other.area()
 
-        The node is inserted into the list at the correct
-        ordered numerical position.
-
-        Args:
-            value (Node): The new Node to insert.
-        """
-        new = Node(value)
-        if self.__head is None:
-            new.next_node = None
-            self.__head = new
-        elif self.__head.data > value:
-            new.next_node = self.__head
-            self.__head = new
-        else:
-            tmp = self.__head
-            while (tmp.next_node is not None and
-                    tmp.next_node.data < value):
-                tmp = tmp.next_node
-            new.next_node = tmp.next_node
-            tmp.next_node = new
-
-    def __str__(self):
-        """Define the print() representation of a SinglyLinkedList."""
-        values = []
-        tmp = self.__head
-        while tmp is not None:
-            values.append(str(tmp.data))
-            tmp = tmp.next_node
-        return ('\n'.join(values))
+    def __ge__(self, other):
+        """Define the >= compmarison to a Square."""
+        return self.area() >= other.area()
