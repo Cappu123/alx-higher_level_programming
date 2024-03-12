4b2a1a4c64b56ae512bb5955a056f2f0ba6e9db5
Cappu123 <cappyalemayehu12@gmail.com>
2024-03-11 18:36:59 -0700

    multic

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined)
+{
+	console.log("Missing number of occurrences");
+}
+else
+{
+	const occurrence = Number(process.argv[2]);
+	let i = 0;
+	while (i < occurrence)
+	{
+		console.log("C is fun");
+		i++;
+	}
+}
+
