7295cad59a4fefdeef64450440012355c429702e
Cappu123 <cappyalemayehu12@gmail.com>
2024-03-16 16:17:51 -0700

    safe

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" takes in an argument
+    displays all values in the states
+    table of hbtn_0e_0_usa
+    where name matches the argument.
+    But this time, write one that is
+    safe from MySQL injections!
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c =db.cursor()
+    match = sys.argv[4]
+    query1 = c.execute("""SELECT * FROM states
+                        WHERE name LIKE BINARY '{}'
+                        ORDER BY states.id ASC""".format(match))
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
