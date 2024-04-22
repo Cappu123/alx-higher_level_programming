55834a4412433eff9173b814b75fba16901fb584
Cappu123 <cappyalemayehu12@gmail.com>
2024-04-22 08:06:38 -0700

    languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
 
-let Three = "C is fun";
-let One = "Python is cool";
-let Two = "JavaScript is amaizing";
-
-console.log(Three);
-console.log(One);
-console.log(Two);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
